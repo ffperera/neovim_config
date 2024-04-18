7a77f75caeb14f1d5da90cd6524c790e1ae36bb4
felipe <felipe@epsilon-eridani.com>
2024-04-18 09:22:42 +0200

    configuracion tabulador global

--- init.lua
+++ init.lua
@@ -3,6 +3,12 @@ require("config.lazy")
 
 vim.cmd.colorscheme("catppuccin")
 
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+
 -- CONTROL+S para guardar
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
