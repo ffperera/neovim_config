641824190850faea617be164a900f70e84febebe
Felipe Fernandez Perera <felipe@epsilon-eridani.com>
2025-05-30 18:46:45 +0200

    neo-tree configuración para mostrar hiddens

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+-- para mostrar ficheros ocultos
+--
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
+}
